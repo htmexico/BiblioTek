cf99fc376c80a01b60e58fa12fd86eb66a46c0e2
jlopez <jlopez@escolarhitech.com>
2014-12-18 19:01:48 +0000

    Cambios a fin de anio

---
+++ config_db.inc.php
@@ -0,0 +1,12 @@
+<?php
+unset($CFG);
+
+global $CFG;
+
+$CFG->db_type    = 'interbase';   // solo interbase
+$CFG->db_host    = 'localhost'; 
+$CFG->db_name    = '/yourpath/biblio.fdb';
+$CFG->db_user    = 'SYSDBA';
+$CFG->db_pass    = '***password***';
+
+ ?>
